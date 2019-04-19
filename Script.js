f1d03a77050d15848a5e1e6ed0169892a8fd9678
tuanpham1406 <phamanhtuan1406@gmail.com>
2019-04-19 15:40:01 +0700

    game đoán số

--- Script.js
+++ Script.js
@@ -1,19 +1,1 @@
-// tìm kiếm nhị phân
-let arr1 = [1,25,50,75,82,100];
 
-arr1.sort(function (a, b) {
-    return a - b;
-});
-
-function binary() {
-    let number  = parseInt(prompt("Enter the number: "));
-
-    for (let i = 0; i < arr1.length; i++) {
-        if (number === 82) {
-            console.log("correct");
-        } else if (number < 82) {
-            console.log("to low");
-        } else console.log("to high");
-        return;
-    }
-}
